a05efa062294f3ef0024763e29120c21143fa50c
palvy1209 <vyom366228@gmail.com>
2024-08-25 16:44:46 +0530

    Create jenkin_installation.sh

---
+++ jenkin_installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "going to install jenkin"
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+echo "installation done!!!"
+echo " check jenkin status"
+sudo systemctl status jenkins
